a4d52310cbaa098eb9099f9a51364ca384c21c4d
徐刚 <gary.xu@qq.com>
2023-04-27 14:59:52 +0800

    add configure file

---
+++ src/config/php-gpt.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'client_id'     => env('CLIENT_ID', 'LTAI4---------44pg8'),
+    'client_secret' => env('CLIENT_SECRET', '9Q7Q---------4Q'),
+    'endpoint'      => env('ENDPOINT', 'https://api.openai.com/v1/engines/davinci/completions'),
+    'url_authorize' => env('URL_AUTHORIZE', 'https://api.openai.com/v1/engines/davinci/completions'),
+    'url_access_token' => env('URL_ACCESS_TOKEN', 'https://api.openai.com/v1/engines/davinci/completions'),
+    'url_resource_owner_details' => env('URL_RESOURCE_OWNER_DETAILS', 'https://api.openai.com/v1/engines/davinci/completions'),
+];
